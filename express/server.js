55ab9e2c566f06a6db5d69b5fefd6490eb88aa9c
JAYANTH PUTHALAPATTU <45038110+Jayanthputhalapattu@users.noreply.github.com>
2020-08-28 00:42:09 +0530

    Update server.js

--- express/server.js
+++ express/server.js
@@ -3,60 +3,20 @@ const express = require('express');
 const path = require('path');
 const serverless = require('serverless-http');
 const app = express();
-var Razorpay = require('razorpay')
 const bodyParser = require('body-parser');
-var Userdata = require('./models/userdata');
-var mongoose = require('mongoose');
-var uri = "mongodb+srv://jayanth:jayanth1610120@cluster0.rdnwp.mongodb.net/Razor?retryWrites=true&w=majority"
-
-var bodyParser = require('body-parser')
-
-app.use(bodyParser.json());
-
+var cors = require('cors')
+app.use(cors())
 const router = express.Router();
-mongoose.connect(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-.then(() => {
-  console.log("MongoDB Connected…")
-})
-.catch(err => console.log(err))
-var instance = new Razorpay({
-  key_id: 'rzp_test_RdkiOl654i5b7N',
-  key_secret: 'OJQTgrrg1QrxN8P2O0LGHyOG',
- 
+router.get('/', (req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.write('<h1>Hello from Express.js!</h1>');
+  res.end();
 });
-router.get('/',(req,res,next)=>{
-    res.send("HELLO WORLD!")
-})
-router.get('/check',(req,res,next)=>{
-  
-  var options = {
-  amount: 100,  // amount in the smallest currency unit
-  currency: "INR",
-  receipt: "order_rcptid_11",
-  payment_capture: '0'
-};
-instance.orders.create(options, function(err, order) {
-  console.log(order);
-  res.send(order)
-});
-})
-router.post("/succeescallback",(req,res,next)=>{
-	   
-    Userdata.create(req.body)
-    .then((user)=>{
-      res.statusCode = 200;
-           res.setHeader('Content-Type','application/json');
-           res.json(user);
-           console.log('inserted succesfully')
-           
-    },err=>next(err))
-    .catch((err)=>next(err))
-})
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
-
+router.post('/', (req, res) => res.json({ postBody: req.body }));
+router.get('/check',(req,res,next)=>{
+    res.json({"hello" : "HELLO WORLD!")
+})
 
 app.use(bodyParser.json());
 app.use('/.netlify/functions/server', router);  // path must route to lambda
